1a3f4e74583b632e6ba3db2b85dec0bef8ca4b9b
jorgeSV <amk114@gmail.com>
2014-02-26 17:54:38 +0100

    Funcionando perfecto

--- LexDroidEjercicio01/src/com/sovejo/lexdroidejercicio01/MainActivity.java
+++ LexDroidEjercicio01/src/com/sovejo/lexdroidejercicio01/MainActivity.java
@@ -9,7 +9,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-public class MainActivity extends Activity implements OnClickListener //No funciona
+public class MainActivity extends Activity implements OnClickListener
 {
 	ImageView apple,android,linux,mozilla,imagenPrincipal;
 	Button boton;
